bacc05c8a685dac8b1c3a1fd03a05e29aae0cb9b
Divya Chaturvedi <chaturvedidivya10@gmail.com>
2025-07-03 13:16:12 -0500

    Updated comment and syntax
    Updated some of the syntax and comments

--- typescript/calculator.ts
+++ typescript/calculator.ts
@@ -14,21 +14,18 @@ type InvestmentResult = {
 
 type CalculationLimit = InvestmentResult[] | string;
 
-function calculateInvestment(data: InvestmentData) : CalculationLimit{
-    const {initialAmount, annualContribution, expectedReturn, duration} = data;
+function calculateInvestment(data: InvestmentData): CalculationLimit {
+    const { initialAmount, annualContribution, expectedReturn, duration } = data;
 
-    if(initialAmount < 0)
-    {
+    if (initialAmount < 0) {
         return 'Initail investment amount should atlest be zero';
     }
 
-    if(duration <= 0)
-    {
+    if (duration <= 0) {
         return 'No valid amount of years provided';
     }
 
-    if(expectedReturn < 0)
-    {
+    if (expectedReturn < 0) {
         return 'Expected return must atlest be zero';
     }
 
@@ -38,15 +35,14 @@ function calculateInvestment(data: InvestmentData) : CalculationLimit{
 
     const annualResults: InvestmentResult[] = [];
 
-    for (let i = 0; i < duration; i++)
-    {
+    for (let i = 0; i < duration; i++) {
         total = total * (1 + expectedReturn);
         totalInterestEarned = total - totalContributions - initialAmount;
         totalContributions = totalContributions + annualContribution;
         total = total + annualContribution;
 
         annualResults.push({
-            year :`Year ${i + 1}`,
+            year: `Year ${i + 1}`,
             totalAmount: total,
             totalInterestEarned,
             totalContributions
@@ -55,25 +51,24 @@ function calculateInvestment(data: InvestmentData) : CalculationLimit{
 
     return annualResults;
 }
+// this function to print results
 
-function PrintResults(results : CalculationLimit){
-    if(typeof results === 'string')
-    {
+function PrintResults(results: CalculationLimit) {
+    if (typeof results === 'string') {
         console.log(results);
         return;
     }
 
-    for( const yearEndResult of results)
-    {
+    for (const yearEndResult of results) {
         console.log(yearEndResult.year);
         console.log(`Total: ${yearEndResult.totalAmount.toFixed(0)}`);
     }
 }
 
-const investmentData : InvestmentData = 
+const investmentData: InvestmentData =
 {
     initialAmount: 5000,
-    annualContribution:500,
+    annualContribution: 500,
     expectedReturn: 0.80,
     duration: 10
 }
